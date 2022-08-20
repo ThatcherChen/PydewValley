16843000ec3a359e9c9e7945fb238ff9dcc8c03f
thatcherchen <thatcherchen@163.com>
2022-08-20 19:59:11 +0800

    Import all elements.

--- code/level.py
+++ code/level.py
@@ -2,7 +2,9 @@ import pygame
 from settings import *
 from player import Player
 from overlay import Overlay
-from sprites import Generic
+from sprites import Generic, Water, WildFlower, Tree
+from pytmx.util_pygame import load_pygame
+from support import import_folder
 
 
 class Level:
@@ -18,6 +20,34 @@ class Level:
 		self.overlay = Overlay(self.player)
 
 	def setup(self):
+		tmx_data = load_pygame('../data/map.tmx')
+
+		# house
+		for layer in ['HouseFloor', 'HouseFurnitureBottom']:
+			for x, y, surf in tmx_data.get_layer_by_name(layer).tiles():
+				Generic((x * TILE_SIZE, y * TILE_SIZE), surf, (self.all_sprites,), LAYERS['house bottom'])
+
+		for layer in ['HouseWalls', 'HouseFurnitureTop']:
+			for x, y, surf in tmx_data.get_layer_by_name(layer).tiles():
+				Generic((x * TILE_SIZE, y * TILE_SIZE), surf, (self.all_sprites,), LAYERS['main'])
+
+		# Fence
+		for x, y, surf in tmx_data.get_layer_by_name('Fence').tiles():
+			Generic((x * TILE_SIZE, y * TILE_SIZE), surf, (self.all_sprites,))
+
+		# Water
+		water_frames = import_folder('../graphics/water')
+		for x, y, surf in tmx_data.get_layer_by_name('Water').tiles():
+			Water((x * TILE_SIZE, y * TILE_SIZE), water_frames, (self.all_sprites,))
+
+		# WildFlowers
+		for obj in tmx_data.get_layer_by_name('Decoration'):
+			WildFlower((obj.x * TILE_SIZE, obj.y * TILE_SIZE), obj.image, (self.all_sprites,))
+
+		# Tree
+		for obj in tmx_data.get_layer_by_name('Trees'):
+			Tree((obj.x, obj.y), obj.image, (self.all_sprites,), obj.name)
+
 		self.player = Player((500, 360), self.all_sprites)
 		Generic(
 			pos=(0, 0),
@@ -45,7 +75,7 @@ class CameraGroup(pygame.sprite.Group):
 		self.offset.y = player.rect.centery - SCREEN_HEIGHT / 2
 
 		for layer in LAYERS.values():
-			for sprite in self.sprites():
+			for sprite in sorted(self.sprites(), key=lambda x: x.rect.centery):
 				if sprite.z == layer:
 					offset_rect = sprite.rect.copy()
 					offset_rect.center -= self.offset
