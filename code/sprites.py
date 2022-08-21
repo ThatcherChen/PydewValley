fd1b2208de86960e1512a617c016dd6b6b6171d7
thatcherchen <thatcherchen@163.com>
2022-08-21 18:54:34 +0800

    Create collision mechanism.

--- code/sprites.py
+++ code/sprites.py
@@ -4,10 +4,11 @@ from settings import *
 
 class Generic(pygame.sprite.Sprite):
     def __init__(self, pos, surf, groups, z=LAYERS['main']):
-        super().__init__(groups[0])
+        super().__init__(groups)
         self.image = surf
         self.rect = self.image.get_rect(topleft=pos)
         self.z = z
+        self.hitbox = self.rect.copy().inflate(-self.rect.width*0.2, -self.rect.height*0.75)
 
 
 class Water(Generic):
@@ -38,6 +39,7 @@ class Water(Generic):
 class WildFlower(Generic):
     def __init__(self, pos, surf, groups):
         super().__init__(pos, surf, groups)
+        self.hitbox = self.rect.copy().inflate(-20, -self.rect.height*0.9)
 
 
 class Tree(Generic):
